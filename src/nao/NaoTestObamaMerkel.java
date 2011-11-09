14d8c7e6876ef9f8d395cc1e29b4963f0217adc9
Christian Denk <denk@ualberta.ca>
2011-11-08 17:41:00 -0700

    First version of obamamerkel

---
+++ src/nao/NaoTestObamaMerkel.java
@@ -0,0 +1,213 @@
+package nao;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import rltoys.algorithms.representations.actions.Action;
+import rltoys.environments.envio.actions.ActionArray;
+import rltoys.environments.envio.observations.TStep;
+import zephyr.plugin.core.api.monitoring.annotations.Monitor;
+import zephyr.plugin.core.api.monitoring.fileloggers.TimedFileLogger;
+import zephyr.plugin.core.api.synchronization.Clock;
+
+//I should add monitoring and logging.
+
+public class NaoTestObamaMerkel {
+  NaoAction na;
+  int i;
+  int j;
+  int mm;
+  double[] joints;
+  double[] stiffness;
+  double[] leds;
+  double[] sounds;
+  int mstep;
+  private final TimedFileLogger logger;
+  private int stage;
+  long lastTime;
+
+  Random random;
+  private final Clock clock;
+  private final NaoRobot R;
+
+  @Monitor
+  ObamaMerkelProblem problem;
+
+  public static void main(String[] args) throws IOException {
+    NaoRobot N = new NaoRobot();
+    new NaoTestSoundEnergyProblem(N, null).run();
+  }
+
+  public NaoTestObamaMerkel(NaoRobot R, Clock clock) throws IOException {
+    System.out.print("Initializing the Robot Interface...\n");
+    na = new NaoAction();
+    this.clock = clock;
+    na.set(null, .1, null, null, null);
+    lastTime = System.currentTimeMillis();
+    this.R = R;
+
+    i = 0;
+    j = 0;
+    mm = 0;
+    mstep = 0;
+    joints = new double[14];
+    for (int i = 0; i < 14; i++)
+      joints[i] = 0.5;
+    stiffness = new double[14];
+
+    logger = new TimedFileLogger("./start" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date()) + ".naolog");
+
+    random = new Random();
+    stage = 0;
+    leds = new double[83];
+    sounds = new double[3];
+    logger.add(R);
+    // Initialise Problem
+    problem = new ObamaMerkelProblem(this.R, 50.0);
+  }
+
+  public void run() {
+    run(new Clock("NaoEnvironment"));
+  }
+
+  public void run(Clock clock) {
+    System.out.println("Entering the run function...");
+    Action[] act = problem.actions();
+
+    while (!clock.isTerminated()) {
+      clock.tick();
+      System.out.print("Motion: " + problem.motion + "\n");
+      problem.update((ActionArray) act[2]);
+    }
+    // // Initialise Problem
+    // @Monitor
+    // SoundEnergyProblem problem = new SoundEnergyProblem(this.R);
+    // // Initialise tilecoder
+    // TileCodersNoHashing tileCoders = new
+    // TileCodersNoHashing(problem.getObservationRanges());
+    // // Add desired amount of tilings:
+    // tileCoders.addFullTilings(5, 3);
+    // // Don't know yet what this means...
+    // TabularAction toStateAction = new TabularAction(problem.actions(),
+    // tileCoders.vectorSize());
+    // // Set parameters for Sarsa
+    // double alpha = .2 / tileCoders.nbActive();
+    // double gamma = 0.0;
+    // double lambda = .3;
+    // // Initialize Sarsa Algorithm:
+    // Sarsa sarsa = new Sarsa(alpha, gamma, lambda, toStateAction.vectorSize(),
+    // new AMaxTraces()); // Hope
+    // // vectorsize
+    // // is
+    // // correct!
+    // double epsilon = 0.01;
+    // // Use epsilon-greedy policy:
+    // Policy acting = new EpsilonGreedy(new Random(0), problem.actions(),
+    // toStateAction, sarsa, epsilon);
+    // // Initialize the sarsa control algorithm:
+    // SarsaControl control = new SarsaControl(acting, toStateAction, sarsa);
+    //
+    //
+    // TRStep step = problem.initialize();
+    // Action action = new ActionArray(0.0, 0.0, 0.0);
+    // RealVector x_t = null;
+    // while (!R.isClosed() && !clock.isTerminated()) {
+    // clock.tick(); // CD: observes all variables for zephyr plot function
+    //
+    // step = problem.step(action);
+    // BinaryVector x_tp1 = tileCoders.project(step.o_tp1);
+    // action = control.step(x_t, step.a_t, x_tp1, step.r_tp1);
+    // x_t = x_tp1;
+    // }
+
+
+  }
+
+  public NaoAction wgetAtp1(TStep step) {
+    // TODO Auto-generated method stub
+    // System.out.println("observation size " + step.o_tp1.length); // image
+    // buffer
+    // not reported
+    // for (int i = 0; i < 60 && i < step.o_tp1.length; i++)
+    // System.out.println(i + " " + step.o_tp1[i]);
+    logger.update();
+    if (i == 0 && j == 0)
+      for (int k = 0; k < 83; k++)
+        leds[k] = 0;
+    leds[i] = j / 7.0;
+    sounds[0] = i + 2;
+    sounds[1] = (j + 1) * 1024;
+    sounds[2] = .5;
+    // joints[mm] = 0.5 + 0.5 * Math.sin(mstep * 2 * Math.PI / 100.0);
+    // stiffness[mm] = .6;
+    na.set(joints, .2, stiffness, leds, sounds);
+
+    joints[mm] = 0.5;
+    stiffness[mm] = 0;
+    System.out.println("Motor: " + mm + " " + mstep + " ij: " + i + " " + j);
+
+    j = (j + 1) % 8;
+    if (j == 0)
+      i = (i + 1) % 83;
+
+    mstep = (mstep + 1) % 100;
+    if (mstep == 0)
+      mm = (mm + 1) % 14;
+
+    return na;
+  }
+
+  public NaoAction getAtp1(double[] obs) {
+    // Performs random action corresponding to current observation and stage...
+    int end = 500;
+    stage++;
+    if (stage < 100) { // gradually stiffen the body to midpoint
+      for (int i = 0; i < stiffness.length; i++)
+        stiffness[i] = 0.8 * stage / 100.0;
+      for (int i = 0; i < leds.length; i++)
+        leds[i] = stage / 100.;
+    } else if (stage < end) {
+      // random walk the joints
+      for (int i = 0; i < stiffness.length; i++) {
+        double val = joints[i];
+        val += (random.nextDouble() - .5) * .04;
+        val = val > .7 ? .7 : val < .4 ? .4 : val;
+        joints[i] = val;
+      }
+      for (int i = 0; i < leds.length; i++) {
+        double val = leds[i];
+        val += (random.nextDouble() - .5) * .1;
+        val = val > 1. ? 1. : val < 0. ? 0. : val;
+        leds[i] = val;
+
+      }
+    } else
+      for (int i = 0; i < stiffness.length; i++)
+        stiffness[i] = 0;
+    // if (stage > end + 50)
+    // return null;
+    long nowTime = System.currentTimeMillis();
+
+
+    System.out.print(" Count:" + stage + " time: " + (nowTime - lastTime));
+    lastTime = nowTime;
+    // for (int i = 0; i < 14; i++)
+    // System.out.print(" " + joints[i]);
+    System.out.println("\n");
+    // joints[mm] = 0.5 + 0.5 * Math.sin(mstep * 2 * Math.PI / 100.0);
+    // stiffness[mm] = .6;
+    na.set(joints, .035, stiffness, leds, null);
+
+
+    System.out.println("Motor: " + mm + " " + mstep + " ij: " + i + " " + j);
+
+    logger.update();
+    if (clock != null)
+      clock.tick();
+
+    return na;
+  }
+
+}
---
+++ src/nao/ObamaMerkelProblem.java
@@ -0,0 +1,124 @@
+package nao;
+
+import rltoys.algorithms.representations.actions.Action;
+import rltoys.environments.envio.actions.ActionArray;
+import rltoys.environments.envio.observations.Legend;
+import rltoys.environments.envio.observations.TRStep;
+import rltoys.environments.envio.problems.ProblemBounded;
+import rltoys.environments.envio.problems.ProblemDiscreteAction;
+import rltoys.math.ranges.Range;
+import zephyr.plugin.core.api.monitoring.annotations.Monitor;
+
+@Monitor
+public class ObamaMerkelProblem implements ProblemBounded, ProblemDiscreteAction {
+  protected static final ActionArray LEFT = new ActionArray(-0.05);
+  protected static final ActionArray RIGHT = new ActionArray(0.05);
+  protected static final ActionArray REST = new ActionArray(0.0);
+  protected static final Action[] Actions = { LEFT, RIGHT, REST };
+
+
+  private double[] leds = new double[83];
+  private final NaoRobot robot;
+  private final NaoAction naoAct = new NaoAction();
+
+
+  double[] obsArray;
+
+  private double reward = 0.0;
+
+  double motion = 0.0;
+  private double oldHeadPosition = 0.0;
+  private double newHeadPosition = 0.0;
+  private final double motionBorder;
+  private final double[] joints = new double[14];
+  private final double[] stiffness = new double[14];
+  private Legend legend;
+
+  // private final PVector soundMagnitudes0 = new PVector(1024);
+  // private final PVector soundMagnitudes1 = new PVector(1024);
+
+
+  public ObamaMerkelProblem(NaoRobot R, double motionBorder) {
+    this.robot = R;
+    naoAct.set(joints, 0.1, stiffness, null, null);
+    robot.sendAction(naoAct);
+    this.motionBorder = motionBorder;
+  }
+
+  protected void update(ActionArray action) {
+    // Get new observations from robot:
+    obsArray = robot.waitNewObs();
+    // // Copy the sound observations to the soundMagnitudes vectors:
+    // for (int n = 0; n < 1024; n++) {
+    // soundMagnitudes0.setEntry(n, obsArray[67 + n]);
+    // soundMagnitudes1.setEntry(n, obsArray[67 + n + 1024]);
+    // }
+    // soundMagnitudes0.mapMultiplyToSelf(0.00001);
+    // soundMagnitudes1.mapMultiplyToSelf(0.00001);
+
+    // get new motion value:
+    motion = robot.getMotion();
+
+    // get new Head Position:
+    newHeadPosition = obsArray[12];
+
+
+    // Calculate Reward:
+    System.out.print("is motion zero? value is... " + (newHeadPosition - oldHeadPosition) + "\n");
+    if (newHeadPosition - oldHeadPosition == 0) {
+      System.out.println("Motion is zero!");
+      if (motion > motionBorder) {
+        reward = 1.0;
+      }
+    } else {
+      reward = 0.0;
+    }
+    oldHeadPosition = newHeadPosition;
+
+
+    // Light LEDs of Nao according to reward:
+    if (reward < 1.0) {
+      leds = NaoAction.setFaceLeds(2);
+    } else {
+      leds = NaoAction.setFaceLeds(1);
+    }
+
+    // Set the desired agent action:
+    joints[0] = action.actions[0];
+    if (joints[0] > 1.0) {
+      joints[0] = 1.0;
+    } else if (joints[0] < -1.0) {
+      joints[0] = -1.0;
+    }
+    // Put these LEDs to the nao...
+    naoAct.set(joints, 0.1, stiffness, leds, null);
+    robot.sendAction(naoAct);
+  }
+
+  @Override
+  public Action[] actions() {
+    return Actions;
+  }
+
+  @Override
+  public Range[] getObservationRanges() {
+    return new Range[] {};
+  }
+
+  @Override
+  public Legend legend() {
+    return legend;
+  }
+
+  @Override
+  public TRStep initialize() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public TRStep step(Action action) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
