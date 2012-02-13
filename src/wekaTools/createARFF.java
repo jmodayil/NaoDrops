b516e4b090b6f91249237bb1a1df75bfd0f98d3a
Christian Denk <denk@ualberta.ca>
2012-02-13 14:08:22 -0700

    Added the Weka Tools package

---
+++ src/wekaTools/createARFF.java
@@ -0,0 +1,118 @@
+package wekaTools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.text.ParseException;
+
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+
+
+public class createARFF {
+
+  /**
+   * @param args
+   * @throws ParseException
+   * @throws IOException
+   */
+  public static void main(String[] args) throws ParseException, IOException {
+
+    double[][][] featureData;
+    FastVector names = new FastVector();
+    for (int n = 0; n < args.length; n++) {
+      names.addElement(new String("Speaker" + n));
+    }
+
+
+    featureData = readInput(args);
+
+    FastVector atts;
+    Instances data;
+    double[] vals;
+
+    for (int n = 0; n < featureData.length; n++) {
+      System.out.println((String) names.elementAt(n));
+    }
+
+    // 1. set up attributes
+    atts = new FastVector();
+    // - numeric
+    for (int n = 0; n < featureData[0][0].length; n++) {
+      atts.addElement(new Attribute("feature" + n));
+    }
+    atts.addElement(new Attribute("class", names));
+
+
+    // 2. create Instances object
+    data = new Instances("MyRelation", atts, 0);
+
+    // 3. fill with data
+    for (int person = 0; person < featureData.length; person++) {
+      for (int sample = 0; sample < featureData[0].length; sample++) {
+        vals = new double[data.numAttributes()];
+        for (int feature = 0; feature < featureData[0][0].length; feature++) {
+          vals[feature] = featureData[person][sample][feature];
+        }
+        vals[data.numAttributes() - 1] = data.attribute(data.numAttributes() - 1).indexOfValue((String) names
+                                                                                                   .elementAt(person));
+        System.out.println("VALS [  ]: " + vals[data.numAttributes() - 1]);
+        data.add(new Instance(1.0, vals));
+      }
+    }
+
+    // Save the data to a file
+
+    ArffSaver saver = new ArffSaver();
+    File outputFile = new File(args[args.length - 1] + ".arff");
+    if (outputFile.exists()) {
+      System.out.println("Output File exists already! ABORTING!");
+      return;
+    }
+    saver.setInstances(data);
+    saver.setFile(outputFile);
+    saver.writeBatch();
+  }
+
+  private static double[][][] readInput(String[] args) {
+    int length = args.length;
+    Double[][][] data = new Double[length][][];
+
+    double[][][] realData = new double[length][][];
+
+
+    FileInputStream fis = null;
+    ObjectInputStream in = null;
+
+    // Read the Files:
+    for (int n = 0; n < length; n++) {
+      try {
+        System.out.println(args[n]);
+        fis = new FileInputStream(args[n]);
+        in = new ObjectInputStream(fis);
+        realData[n] = ((double[][]) in.readObject());
+        in.close();
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      } catch (ClassNotFoundException ex) {
+        ex.printStackTrace();
+      }
+    }
+
+    // for (int m = 0; m < data.length; m++) {
+    // realData[m] = new double[data[0].length][];
+    // for (int n = 0; n < data[0].length; n++) {
+    // realData[m][n] = ArrayUtils.toPrimitive(data[m][n]);
+    // }
+    // }
+
+    System.out.println("Dimensions of feature Data: " + realData.length + " " + realData[realData.length - 1].length
+        + " " + realData[realData.length - 1][realData[realData.length - 1].length - 1].length);
+
+    return realData;
+  }
+}
